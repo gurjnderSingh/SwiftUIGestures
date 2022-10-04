479600e2853b06e5615183b2dbe31eca1ee396e5
Gurjinder Singh <gurjindersingh663@gmail.com>
2022-10-04 23:08:06 +0530

    project created

---
+++ SwiftUiGestures/SwiftUiGestures/SwiftUiGesturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiGesturesApp.swift
+//  SwiftUiGestures
+//
+//  Created by Gurjinder Singh on 04/10/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiGesturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
