f8715a7c20e710940fd1dfc454ce3a106c8b3298
Gurjinder Singh <gurjindersingh663@gmail.com>
2022-10-05 21:24:53 +0530

    image added, double tap gesture added to image and scale effect added to image

--- SwiftUiGestures/SwiftUiGestures/Screen/ContentView.swift
+++ SwiftUiGestures/SwiftUiGestures/Screen/ContentView.swift
@@ -8,14 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    // MAKE: - Property
+    @State private var isAnimating: Bool = false
+    @State private var imageScale: CGFloat = 1
+    // MAKE: - Function
+    // MAKE: - Content
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        NavigationView {
+            ZStack {
+                Image("magazine-front-cover")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(8)
+                    .padding()
+                    .shadow(color: .black.opacity(0.8), radius: 12, x: 2, y: 2)
+                    .opacity(isAnimating ? 1 : 0)
+                    .animation(.linear(duration: 1), value: isAnimating)
+                    .scaleEffect(imageScale)
+                    .onTapGesture(count: 2, perform: {
+                        if imageScale == 1 {
+                            withAnimation(.spring()) {
+                                imageScale = 5
+                            }
+                        } else {
+                            withAnimation(.spring()) {
+                                imageScale = 1
+                            }
+                        }
+                    })
+            }
+            .navigationTitle("Pinch & Zoom")
+            .navigationBarTitleDisplayMode(.inline)
+            .onAppear {
+                    isAnimating = true
+            }
+        } // END of Navigation View
+        //.navigationViewStyle(.stack)
     }
 }
 
