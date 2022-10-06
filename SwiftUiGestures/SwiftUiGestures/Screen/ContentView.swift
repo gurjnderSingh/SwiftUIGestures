04c6aaeb289057006c8dc3dc0d713c63b4c60d46
Gurjinder Singh <gurjindersingh663@gmail.com>
2022-10-06 08:24:21 +0530

    drag gesture added to image

--- SwiftUiGestures/SwiftUiGestures/Screen/ContentView.swift
+++ SwiftUiGestures/SwiftUiGestures/Screen/ContentView.swift
@@ -8,11 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
-    // MAKE: - Property
+    // MARK: - Property
+    
     @State private var isAnimating: Bool = false
     @State private var imageScale: CGFloat = 1
-    // MAKE: - Function
-    // MAKE: - Content
+    @State private var imageOffset: CGSize = .zero
+    
+    // MARK: - Function
+    
+    func resetImageState() {
+        return withAnimation(.spring()) {
+            imageScale = 1
+            imageOffset = .zero
+        }
+    }
+    
+    // MARK: - Content
+    
     var body: some View {
         NavigationView {
             ZStack {
@@ -24,19 +36,34 @@ struct ContentView: View {
                     .shadow(color: .black.opacity(0.8), radius: 12, x: 2, y: 2)
                     .opacity(isAnimating ? 1 : 0)
                     .animation(.linear(duration: 1), value: isAnimating)
+                    .offset(imageOffset)
                     .scaleEffect(imageScale)
+                // MARK: - 1. Tap Gesture
                     .onTapGesture(count: 2, perform: {
                         if imageScale == 1 {
                             withAnimation(.spring()) {
                                 imageScale = 5
                             }
                         } else {
-                            withAnimation(.spring()) {
-                                imageScale = 1
-                            }
+                           resetImageState()
                         }
                     })
+                // MARK: - 2. Drag Gesture
+                    .gesture(
+                        DragGesture()
+                            .onChanged({ gesture in
+                                withAnimation(.linear(duration: 1)) {
+                                    imageOffset = gesture.translation
+                                }
+                            })
+                            .onEnded({ _ in
+                                if imageScale <= 1 {
+                                    resetImageState()
+                                }
+                            })
+                    )
             }
+            .ignoresSafeArea(edges: .all)
             .navigationTitle("Pinch & Zoom")
             .navigationBarTitleDisplayMode(.inline)
             .onAppear {
